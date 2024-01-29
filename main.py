0f54ae97fc883f065b893fe798cf8968acd629db
Jack Cerne <snax.2@hotmail.com>
2024-01-30 10:22:59 +1100

    Level initial setup

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame, sys
 from settings import *
-
+from level import Level
 
 #pygame setup
 pygame.init()
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map,screen)
+
 
 while True:
     for event in pygame.event.get():
@@ -15,6 +17,6 @@ while True:
             sys.exit()
 
     screen.fill('black')
-
+    level.run()
     pygame.display.update()
     clock.tick(60)
