f0a303e61555119b793161c9b0d3380ec1842886
Jack Cerne <snax.2@hotmail.com>
2024-02-02 22:31:09 +1100

    collectables animated

--- Level/level.py
+++ Level/level.py
@@ -1,15 +1,34 @@
 import pygame
-from support import import_csv_layout
+from support import import_csv_layout, import_graphics
 from settings import tile_size
-from tiles import Tile
+from tiles import Tile, StaticTile, Coin
 
 class Level:
     def __init__(self,level_data,surface):
+        #General Setup
         self.display_surface = surface
+        self.world_shift = 0
 
-
+        #Platform Setup
         platform_layout = import_csv_layout(level_data['Platforms'])
         self.platform_sprites = self.create_tile_group(platform_layout,'Platform')
+
+
+        #Collectables
+        collectables_layout = import_csv_layout(level_data['Collectables'])
+        self.collectables_sprites = self.create_tile_group(collectables_layout,'Collectables')
+
+        #Enemies must change
+        #platform_layout = import_csv_layout(level_data['Enemies'])
+        #self.platform_collectables = self.create_tile_group(platform_layout, 'Enemies')
+
+        #Start/Stop must change
+        #platform_layout = import_csv_layout(level_data['Start/Stop'])
+        #self.platform_collectables = self.create_tile_group(platform_layout, 'Start/Stop')
+
+        #Constraints must change
+        #platform_layout = import_csv_layout(level_data['Constraints'])
+        #self.platform_collectables = self.create_tile_group(platform_layout, 'Constraints')
 
     def create_tile_group(self,layout,type):
         sprite_group = pygame.sprite.Group()
@@ -21,8 +40,18 @@ class Level:
                     y = row_index * tile_size
 
                     if type == 'Platform':
-                        sprite = Tile(tile_size,x,y)
-                        sprite_group.add(sprite)
+                        platform_tile_list = import_graphics('/Users/snax/Desktop/SUPER BART/Level/Level/Graphics/tiles-6 2.png')
+                        tile_surface = platform_tile_list[int(val)]
+                        sprite = StaticTile(tile_size,x,y,tile_surface)
+
+
+
+                    if type == 'Collectables':
+                            if val == '0': sprite = Coin(tile_size,x,y,'/Users/snax/Desktop/SUPER BART/Level/Level/Graphics/Coin')
+                            if val == '4': sprite = Coin(tile_size,x,y,'/Users/snax/Desktop/SUPER BART/Level/Level/Graphics/Coin')
+                            #change above to star after creating png files
+
+                    sprite_group.add(sprite)
 
         return sprite_group
 
@@ -30,4 +59,14 @@ class Level:
 
     def run(self):
         #Run game
+
+        #Platforms
+        self.platform_sprites.update(self.world_shift)
         self.platform_sprites.draw(self.display_surface)
+
+
+        #Collectables
+        self.collectables_sprites.update(self.world_shift)
+        self.collectables_sprites.draw(self.display_surface)
+
+
--- Level/support.py
+++ Level/support.py
@@ -1,4 +1,20 @@
 from csv import reader
+from settings import tile_size
+import pygame.image
+import pygame
+from os import walk
+
+def import_folder(path):
+    surface_list = []
+    for _,__,image_files in walk(path):
+        for image in image_files:
+            full_path = path + '/' + image
+            image_surface = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surface)
+
+    return surface_list
+
+
 
 def import_csv_layout(path):
     platform_map = []
@@ -7,3 +23,23 @@ def import_csv_layout(path):
         for row in level:
             platform_map.append(list(row))
         return platform_map
+
+def import_graphics(path):
+    surface = pygame.image.load(path).convert_alpha()
+    tile_num_x = int(surface.get_size()[0] / tile_size)
+    tile_num_y = int(surface.get_size()[1] / tile_size)
+
+    cut_tiles = []
+    for row in range(tile_num_y):
+        for col in range(tile_num_x):
+            x = col * tile_size
+            y = row * tile_size
+            new_surface = pygame.Surface((tile_size,tile_size))
+            new_surface.blit(surface, (0,0),pygame.Rect(x,y,tile_size,tile_size))
+            cut_tiles.append(new_surface)
+
+    return cut_tiles
+
+
+
+
