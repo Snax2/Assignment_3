97c81d2e9da87329990d82d7b0ce4a20f6a29ed1
Jack Cerne <snax.2@hotmail.com>
2024-02-03 20:36:59 +1100

    token player token for overworld

--- overworld/Overworld.py
+++ overworld/Overworld.py
@@ -12,6 +12,13 @@ class Node(pygame.sprite.Sprite):
             self.image.fill('red')
         self.rect = self.image.get_rect(center = pos)
 
+class Token(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((20,20))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(center = pos)
+
 
 
 class Overworld:
@@ -24,6 +31,12 @@ class Overworld:
 
         #sprites
         self.setup_nodes()
+        self.setup_token()
+
+    def setup_token(self):
+        self.token = pygame.sprite.GroupSingle()
+        token_sprite = Token(self.nodes.sprites()[self.current_level].rect.center)
+        self.token.add(token_sprite)
 
     def setup_nodes(self):
         self.nodes = pygame.sprite.Group()
@@ -35,12 +48,8 @@ class Overworld:
                 node_sprite = Node(node_data['node_pos'],'locked')
             self.nodes.add(node_sprite)
 
-
-
-
-
     def draw_path(self):
-        points = [node['node_pos']for node in levels.values()]
+        points = [node['node_pos'] for index, node in enumerate(levels.values()) if index <= self.max_level]
         pygame.draw.lines(self.display_surface,'green',False,points,5)
 
 
@@ -49,3 +58,5 @@ class Overworld:
     def run(self):
         self.draw_path()
         self.nodes.draw(self.display_surface)
+        self.token.draw(self.display_surface)
+
